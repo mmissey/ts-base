9dc8b6b0f3e77ed27fb8d52bba4c6fd486417a5e
Marc Missey <marcmissey@gmail.com>
2019-06-23 13:21:58 -0600

    feat: add i18n

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './pages/App';
 import { Provider } from 'react-redux';
 import store from './state/store';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './i18n';
 
 ReactDOM.render(
 <Provider store={store}>
