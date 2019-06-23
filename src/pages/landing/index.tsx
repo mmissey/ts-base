1c3c66e53fc6945e9c03cbe80243190d476c0c7e
Marc Missey <marcmissey@gmail.com>
2019-06-23 16:18:53 -0600

    feat: remove landing page

--- src/pages/landing/index.tsx
+++
@@ -1,7 +0,0 @@
-import Loadable from 'react-loadable';
-import LoadingPage from '../../_common/LoadingPage';
-
-export default Loadable({
-  loader: () => import('./page'),
-  loading: LoadingPage,
-});
