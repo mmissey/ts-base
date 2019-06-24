17d81c70fe091dea38ac720a66b6550fabb20a0c
Marc Missey <marcmissey@gmail.com>
2019-06-23 21:39:58 -0600

    feat: add more epics

--- src/state/rootEpic.ts
+++ src/state/rootEpic.ts
@@ -1,7 +1,10 @@
 /* eslint-disable import/prefer-default-export */
 import images from './images/epics';
-
+import users from './users/epics';
+import galleries from './galleries/epics';
 
 export default [
-  ...images
+  ...images,
+  ...galleries,
+  ...users
 ];
